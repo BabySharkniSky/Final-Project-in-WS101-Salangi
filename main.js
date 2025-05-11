c3251e28ec7487bea48633f3fd53758fe1f296a8
BabySharkniSky <charmainesoriano35@gmail.com>
2025-05-12 02:05:04 +0800

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-    <script>
-document.querySelector('.mobile-menu-toggle').addEventListener('click', function() {
-    document.querySelector('.navbar').classList.toggle('active');
-});
-        </script>
