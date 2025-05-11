f0bfc35b5fb83caa849b71a91e2ee3fe78437599
BabySharkniSky <charmainesoriano35@gmail.com>
2025-05-12 02:20:51 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-<script>
+
 document.querySelector('.mobile-menu-toggle').addEventListener('click', function() {
     document.querySelector('.navbar').classList.toggle('active');
 });
-        </script>
+  
 
 
