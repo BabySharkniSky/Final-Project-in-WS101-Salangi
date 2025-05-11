04ec27051996e836cbc20fb47cad9e895cdd30e2
BabySharkniSky <charmainesoriano35@gmail.com>
2025-05-11 23:45:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+<script>
+const slides = document.querySelectorAll('.hero-image img');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+setInterval(nextSlide, 3500); 
+</script>
+
+
