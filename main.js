c1b6a0cb3526593c374396effeaab6e1816406c4
BabySharkniSky <charmainesoriano35@gmail.com>
2025-05-12 01:54:19 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
-<script>
-const slides = document.querySelectorAll('.hero-image img');
-let currentSlide = 0;
-
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-    });
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-}
-
-showSlide(currentSlide);
-setInterval(nextSlide, 3500); 
-</script>
-
-
+    <script>
+document.querySelector('.mobile-menu-toggle').addEventListener('click', function() {
+    document.querySelector('.navbar').classList.toggle('active');
+});
+        </script>
